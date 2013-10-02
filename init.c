ff850b13c6810885d87fd2de76ae3c7e4e8f957e
Fotis Koutoulakis <fotis.koutoulakis@gmail.com>
2013-10-03 01:57:44 +0300

    [REFACTOR] Changed init's coding style to GNU style and made some changes to its logic

--- init.c
+++ init.c
@@ -1,4 +1,8 @@
-// init: The initial user-level program
+/*
+ * init: The initial user-level program
+ * For now, it spawns a shell, and that's
+ * all.
+ */
 
 #include "types.h"
 #include "include/stat.h"
@@ -8,30 +12,44 @@
 char *argv[] = { "sh", 0 };
 
 int
-main(void)
+main (void)
 {
-  int pid, wpid;
+  int pid, 
+      wpid;
 
-  if(open("console", O_RDWR) < 0){
-    mknod("console", 1, 1);
-    open("console", O_RDWR);
-  }
-  dup(0);  // stdout
-  dup(0);  // stderr
+  /* Here, init tries to create a new
+   * console device, and then opens it
+   * as file descriptors 0, 1, and 2
+   */
+  if (open ("console", O_RDWR) < 0) 
+    {
+      mknod ("console", 1, 1);
+      open ("console", O_RDWR);
+    }
+  dup (0);  // stdout
+  dup (0);  // stderr
 
-  for(;;){
-    printf(1, "init: starting sh\n");
-    pid = fork();
-    if(pid < 0){
-      printf(1, "init: fork failed\n");
-      exit();
-    }
-    if(pid == 0){
-      exec("sh", argv);
-      printf(1, "init: exec sh failed\n");
-      exit();
-    }
-    while((wpid=wait()) >= 0 && wpid != pid)
-      printf(1, "zombie!\n");
+  for (;;)
+    {
+      printf (1, "init: starting sh\n");
+      pid = fork ();
+      
+      if (pid == 0)
+        {
+          exec ("sh", argv);
+
+          /* [!!] This code gets executed if exec fails */
+          printf (1, "init: exec sh failed\n");
+          exit ();
+        }
+      else if (pid < 0) 
+        {
+          printf (1, "init: fork failed\n");
+          exit ();
+        }
+
+      /* Here init handles orphaned zombies until the shell exits */
+      while ((wpid = wait ()) >= 0 && wpid != pid)
+        printf (1, "zombie!\n");
   }
 }
